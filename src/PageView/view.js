17151aefcaa1ba1ca9794f7f52f6bcdd779cbd5d
Filimon <filimonhaftu18@gmail.com>
2022-06-06 16:33:38 +0200

    splited the code

---
+++ src/PageView/view.js
@@ -0,0 +1,50 @@
+import {searchList, resultGrid} from '../constant.js';
+import {loadDetails} from '../fetch-data.js';
+
+export function displayMovieList(movies){
+    let moviePoster;
+    searchList.innerHTML = "";
+    for(let i = 0; i < movies.length; i++){
+        let movieListItem = document.createElement('div');
+        movieListItem.dataset.id = movies[i].imdbID;
+        movieListItem.classList.add('search-list-item');
+        if(movies[i].Poster != "N/A"){
+            moviePoster = movies[i].Poster;
+        }else { 
+            moviePoster = "image_not_found.png";
+        }
+        movieListItem.innerHTML = `
+        <div class = "search-item">
+            <img src = "${moviePoster}">
+        </div>
+        <div class = "search-item-info">
+            <h3>${movies[i].Title}</h3>
+            <p>${movies[i].Year}</p>
+        </div>
+        `;
+        searchList.appendChild(movieListItem);
+    }
+    loadDetails();
+}
+
+export function displayMovieDetails(details){
+    resultGrid.innerHTML = `
+    <div class = "movie-poster">
+        <img src = "${(details.Poster != "N/A") ? details.Poster : "image_not_found.png"}" alt = "movie poster">
+    </div>
+    <div class = "movie-info">
+        <h3>${details.Title}</h3>
+        <ul>
+            <li class = "year">Year: ${details.Year}</li>
+            <li class = "rated">Ratings: ${details.Rated}</li>
+            <li class = "released">Released: ${details.Released}</li>
+        </ul>
+        <p class = "genre"><b>Genre:</b> ${details.Genre}</p>
+        <p class = "writer"><b>Writer:</b> ${details.Writer}</p>
+        <p class = "actors"><b>Actors: </b>${details.Actors}</p>
+        <p class = "plot"><b>Plot:</b> ${details.Plot}</p>
+        <p class = "language"><b>Language:</b> ${details.Language}</p>
+        <p class = "awards"><b><i class = "fas fa-award"></i></b> ${details.Awards}</p>
+    </div>
+    `;
+}
